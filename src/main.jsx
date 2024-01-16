f621db8b018aeab3e0953132b27edcf5db2199ec
João Paulo <joaopaulomayka@gmail.com>
2024-01-16 16:58:56 -0300

    creating GlobalStyle

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './templates/App/index'
+import { GlobalStyles } from './styles/global-styles'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyles/>
     <Home />
   </React.StrictMode>,
 )
