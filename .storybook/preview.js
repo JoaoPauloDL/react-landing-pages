b9d97c76f67c9727112d3a2553bcbdb95e3403c7
João Paulo <joaopaulomayka@gmail.com>
2024-01-17 17:52:42 -0300

    feat: testing storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../src/styles/global-styles';
+import { theme } from '../src/styles/theme';
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -9,6 +14,16 @@ const preview = {
       },
     },
   },
-};
+
+}
+
+export const decorators= [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story/>
+      <GlobalStyles/>
+    </ThemeProvider>
+  )
+]
 
 export default preview;
